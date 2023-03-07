c36ffe216f2562841e7ef9c61510993a97a70b4e
Ali Raza <aliraza.12748g@gmail.com>
2023-03-07 09:07:07 +0500

    create screen for product listing and single Product Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,29 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
-import {Link} from 'react-router-dom'
-const Product = ({product}) => {
-    console.log(product)
+
+const Product = ({ product }) => {
   return (
-    <Card className='my-3'>
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top' />
-        </Link>
+    <Card className='my-3 p-3 rounded'>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+
       <Card.Body>
-        <Link to={`/product/${product._id}`}>
-            <Card.Title as='div'>
-                <strong>{Product.name}</strong>
-            </Card.Title>
-        </Link>
-      <Card.Text>
-        <div className='my-3'>
-            <Rating value={product.rating} text={`${product.numReviews} Reviews`}/>
-            
-        </div>
-      </Card.Text>
-      <Card.Text as='h3'>
-        ${product.price}
-      </Card.Text>
+        <Card.Link href={`/product/${product._id}`}>
+          <Card.Title >
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Card.Link>
+
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import { Products } from "../product";
+const ProductScreen = ({ match }) => {
+  const product = Products.find((p) => p._id == match.params.id);
 
-const ProductScreen = () => {
   return (
-    <div>
-      Product Screen
-    </div>
-  )
-}
+    <>
+   <Button><a href="/" style={{color:'white'}}>Go Back</a></Button>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  disable={product.countInStock === 0}
+                  className="btn-block"
+                  type="button"
+                >
+                  ADD TO CART
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
