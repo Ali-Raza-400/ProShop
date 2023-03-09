a33e2bbbab3f86db400307d55a35b8c8bd6455cb
Ali Raza <aliraza.12748g@gmail.com>
2023-03-09 09:14:46 +0500

    create db connection and use color pkg for showing color logs in console ...

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
-import express from 'express'
-import Products from './data/product.js'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
-app.get("/",(req,res)=>{
-    res.send("API is Running");
-})
-app.get("/api/products",(req,res)=>{
-    res.send(Products);
-})
-app.get("/api/product/:id",(req,res)=>{
-    const product =Products.find((p)=>p._id =req.params.id)
-    res.send(product);
-})
-const port=process.env.PORT ||4000
-app.listen(4000,console.log(`Listen port in ${process.env.NODE_ENV} at ${port}`))
+import express from "express";
+import Products from "./data/product.js";
+import color from 'colors'
+import dotenv from "dotenv";
+import ConnectDB from "./conn/db.js";
+dotenv.config();
+const app = express();
+ConnectDB();
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+app.get("/api/products", (req, res) => {
+  res.send(Products);
+});
+app.get("/api/product/:id", (req, res) => {
+  const product = Products.find((p) => (p._id = req.params.id));
+  res.send(product);
+});
+const port = process.env.PORT || 4000;
+app.listen(
+  4000,
+  console.log(`Listen port in ${process.env.NODE_ENV} at ${port}`.yellow.bold )
+);
